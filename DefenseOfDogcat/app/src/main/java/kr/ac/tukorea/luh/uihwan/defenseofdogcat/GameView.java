799b2b4add7f7099880533c5d2202bf02d44c647
Lee Ui Hwan <luh5063014@naver.com>
2024-04-06 20:21:20 +0900

    [1주차] GameView 생성 및 리소스 이미지 배치

---
+++ DefenseOfDogcat/app/src/main/java/kr/ac/tukorea/luh/uihwan/defenseofdogcat/GameView.java
@@ -0,0 +1,93 @@
+package kr.ac.tukorea.luh.uihwan.defenseofdogcat;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class GameView extends View {
+
+    private static final String TAG = GameView.class.getSimpleName();
+
+    private Bitmap bgBitmap;
+    private Bitmap controlBitmap;
+
+    private int curStageIDX = 0;
+    private static int[] STAGE_IDS = new int[] {
+            R.mipmap.scene03_background_type_1,
+    };
+
+    public GameView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+
+        // StateBar 제거
+        setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        Resources res = getResources();
+        bgBitmap = BitmapFactory.decodeResource(res, STAGE_IDS[curStageIDX]);
+        controlBitmap = BitmapFactory.decodeResource(res, R.mipmap.scene03_ui_background);
+    }
+
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        float view_ratio = (float)w / (float)h;
+        float game_ratio = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+        if (view_ratio > game_ratio) {
+            transformOffset.set((w - h * game_ratio) / 2, 0);
+            transformScale = h / SCREEN_HEIGHT;
+        } else {
+            transformOffset.set(0, (h - w / game_ratio) / 2);
+            transformScale = w / SCREEN_WIDTH;
+        }
+    }
+
+    private static final float SCREEN_WIDTH = 16.0f;
+    private static final float SCREEN_HEIGHT = 9.0f;
+    private final PointF transformOffset = new PointF();
+    private float transformScale;
+    RectF bgRect = new RectF(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    RectF controlRect = new RectF(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.save();
+        canvas.translate(transformOffset.x, transformOffset.y);
+        canvas.scale(transformScale, transformScale);
+        canvas.drawBitmap(bgBitmap, null, bgRect, null);
+        canvas.drawBitmap(controlBitmap, null, controlRect, null);
+        canvas.restore();
+    }
+
+    private void drawPaint(Canvas canvas, float tx, float ty, float scale, int depth) {
+
+    }
+
+}

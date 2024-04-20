e3cb10218d72f4c7e781ed729fc130a346acdaed
Lee Ui Hwan <luh5063014@naver.com>
2024-04-20 14:16:09 +0900

    [3주차] object pool 적용

--- DefenseOfDogcat/app/src/main/java/kr/ac/tukorea/luh/uihwan/defenseofdogcat/game/Friendly.java
+++ DefenseOfDogcat/app/src/main/java/kr/ac/tukorea/luh/uihwan/defenseofdogcat/game/Friendly.java
@@ -2,6 +2,8 @@ package kr.ac.tukorea.luh.uihwan.defenseofdogcat.game;
 
 import android.graphics.RectF;
 
+import java.util.ArrayList;
+
 import kr.ac.tukorea.luh.uihwan.defenseofdogcat.R;
 import kr.ac.tukorea.luh.uihwan.framework.interfaces.IBoxCollidable;
 import kr.ac.tukorea.luh.uihwan.framework.objects.AnimSprite;
@@ -15,13 +17,24 @@ public class Friendly extends AnimSprite implements IBoxCollidable {
             R.mipmap.unit_01_rat_animation_sheet, R.mipmap.unit_02_rabbit_animation_sheet, R.mipmap.unit_03_bear_animation_sheet,
     };
 
+    protected static ArrayList<Friendly> objPool = new ArrayList<>();
+
     private Friendly(int index, int frameCount) {
         super(resIds[index], ANIM_FPS, frameCount);
         setPosition(1.0f, 4.0f, 1.5f, 1.5f);
         dx = SPEED;
     }
     public static Friendly get(int index, int frameCount) {
+        if (!objPool.isEmpty()) {
+            Friendly friendly = objPool.remove(0);
+            friendly.setPosition(1.0f, 4.0f, 1.5f, 1.5f);
+            return friendly;
+        }
         return new Friendly(index, frameCount);
+    }
+
+    public void addToPool() {
+        objPool.add(this);
     }
 
     @Override
@@ -29,6 +42,7 @@ public class Friendly extends AnimSprite implements IBoxCollidable {
         super.update(elapsedSeconds);
         if (dstRect.right > Metrics.width) {
             Scene.top().remove(this);
+            objPool.add(this);
         }
     }
 

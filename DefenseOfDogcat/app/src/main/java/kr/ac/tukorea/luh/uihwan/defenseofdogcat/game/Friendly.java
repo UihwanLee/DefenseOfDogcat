465122212f30d34b86fb93c1b7efea73b0cddba1
Lee Ui Hwan <luh5063014@naver.com>
2024-04-20 12:40:19 +0900

    [3주차] 화면 밖으로 나가면 객체를 gameObjects 에서 삭제한다.

--- DefenseOfDogcat/app/src/main/java/kr/ac/tukorea/luh/uihwan/defenseofdogcat/game/Friendly.java
+++ DefenseOfDogcat/app/src/main/java/kr/ac/tukorea/luh/uihwan/defenseofdogcat/game/Friendly.java
@@ -1,13 +1,23 @@
 package kr.ac.tukorea.luh.uihwan.defenseofdogcat.game;
 
 import kr.ac.tukorea.luh.uihwan.framework.objects.Sprite;
+import kr.ac.tukorea.luh.uihwan.framework.scene.Scene;
+import kr.ac.tukorea.luh.uihwan.framework.view.Metrics;
 
 public class Friendly extends Sprite {
-    private static final float SPEED = 3.0f;
+    private static final float SPEED = 1.0f;
 
     public Friendly(int index) {
         super(index);
         setPosition(1.0f, 2.0f, 1.5f, 1.5f);
         dx = SPEED;
+    }
+
+    @Override
+    public void update(float elapsedSeconds) {
+        super.update(elapsedSeconds);
+        if (dstRect.right > Metrics.width) {
+            Scene.top().remove(this);
+        }
     }
 }

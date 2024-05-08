9e9a854fb40af88bb7ff5ce8cd59c8803b991f92
Lee Ui Hwan <luh5063014@naver.com>
2024-05-08 16:08:57 +0900

    [6주차] AllySlotButton 구현

--- DefenseOfDogcat/app/src/main/java/kr/ac/tukorea/luh/uihwan/defenseofdogcat/game/InGameScene.java
+++ DefenseOfDogcat/app/src/main/java/kr/ac/tukorea/luh/uihwan/defenseofdogcat/game/InGameScene.java
@@ -6,6 +6,7 @@ import android.view.MotionEvent;
 
 import kr.ac.tukorea.luh.uihwan.defenseofdogcat.R;
 import kr.ac.tukorea.luh.uihwan.framework.objects.Background;
+import kr.ac.tukorea.luh.uihwan.framework.objects.Button;
 import kr.ac.tukorea.luh.uihwan.framework.objects.JoyStick;
 import kr.ac.tukorea.luh.uihwan.framework.res.Sound;
 import kr.ac.tukorea.luh.uihwan.framework.scene.Scene;
@@ -20,8 +21,18 @@ public class InGameScene extends Scene {
     RectF bgRect = new RectF(0, 0, Metrics.width, Metrics.height);
     RectF controlRect = new RectF(0, 0, Metrics.width, Metrics.height);
 
+    private static int[] UI_ALLY_IDS = new int[] {
+            R.mipmap.ui_ally_01_rat,
+            R.mipmap.ui_ally_02_rabbit,
+            R.mipmap.ui_ally_03_bear,
+            R.mipmap.ui_ally_04_turtle,
+            R.mipmap.ui_ally_05_rhinoceros,
+            R.mipmap.ui_ally_06_penguin,
+            R.mipmap.ui_ally_07_dragon,
+    };
+
     public enum Layer {
-        bg, player, enemy, friendly, controller, UI, COUNT
+        bg, player, enemy, friendly, controller, UI, touch, COUNT
     }
 
     public InGameScene(int stage) {
@@ -32,7 +43,7 @@ public class InGameScene extends Scene {
         add(Layer.bg, new Background(stage));
 
         // Unit Generator 생성
-        //add(Layer.controller, new EnemyGenerator());
+        add(Layer.controller, new EnemyGenerator());
         //add(Layer.controller, new CollisionChecker());
 
         // joyStick 초기화
@@ -48,6 +59,23 @@ public class InGameScene extends Scene {
 
         // UI 생성
         add(Layer.UI, new Background(R.mipmap.scene03_ui_background));
+
+        // Button 생성
+        createAllyButton();
+    }
+
+    private void createAllyButton()
+    {
+        for(int i=0; i<UI_ALLY_IDS.length; i++)
+        {
+            add(InGameScene.Layer.touch, new Button(UI_ALLY_IDS[i], i * 1.6f + 1.1f, 6.7f, 1.6f, 1.6f, new Button.Callback() {
+                @Override
+                public boolean onTouch(Button.Action action) {
+
+                    return true;
+                }
+            }));
+        }
     }
 
     @Override
